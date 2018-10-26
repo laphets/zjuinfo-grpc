26a3bf69c642b3f8350e9823430db0f34a57ab04
laphets <i@laphets.com>
2018-10-26 14:44:19 +0800

    Update gitignore

--- client.js
+++ client.js
@@ -2,7 +2,7 @@
  * @Author: Laphets 
  * @Date: 2018-04-22 00:42:03 
  * @Last Modified by: Laphets
- * @Last Modified time: 2018-10-25 16:57:06
+ * @Last Modified time: 2018-10-25 18:08:17
  */
 
 const PROTO_PATH = __dirname + '/protos/zju_auth.proto';
@@ -11,7 +11,7 @@ const grpc = require('grpc');
 let protoDescriptor = grpc.load(PROTO_PATH);
 let qscrpc = protoDescriptor.qscrpc;
 
-let client = new qscrpc.ZJUauth(`0.0.0.0:50052`, grpc.credentials.createInsecure());
+let client = new qscrpc.ZJUauth(`10.202.68.181:8890`, grpc.credentials.createInsecure());
 
 const user = require('./test_user').test_user;
 
